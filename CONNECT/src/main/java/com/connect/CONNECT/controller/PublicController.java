2f4ab1aa7ae494171842d970dc9a4e47a501a774
HamzaShaikh7 <imhamzase7en@gmail.com>
2025-06-02 19:17:46 +0530

    /public/health-check API configuration.

---
+++ CONNECT/src/main/java/com/connect/CONNECT/controller/PublicController.java
@@ -0,0 +1,21 @@
+package com.connect.CONNECT.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    private static final String HEALTH_CHECK_SUCCESS = "OK";
+
+    @GetMapping("/health-check")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>(HEALTH_CHECK_SUCCESS, HttpStatus.OK);
+    }
+}
