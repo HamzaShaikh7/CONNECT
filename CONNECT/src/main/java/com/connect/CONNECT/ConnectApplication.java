4ec53407bf26bf61b9399239842fb2614b3f0bd4
HamzaShaikh7 <imhamzase7en@gmail.com>
2025-06-05 20:49:37 +0530

    External API integration and Redis Implementation. @value and @PostConstruct implementation. Working: 1. Configure Redis server. 2. Get data from External API. (http://api.weatherstack.com/). 3. Get response from API and store in Redis. 4. Get data from Redis.
    ====================================================================
    Check Redis Entry in WSL.
    
    oracle@DESKTOP-SAP6G6J:~$ sudo service redis-server start
    [sudo] password for oracle: Oracle@123
    oracle@DESKTOP-SAP6G6J:~$
    oracle@DESKTOP-SAP6G6J:~$ redis-cli
    127.0.0.1:6379>
    127.0.0.1:6379> get weather_of_mumbai
    "{\"current\":{\"temperature\":29,\"feelslike\":34,\"weather_descriptions\":[\"Partly Cloudy \"]}}"
    127.0.0.1:6379>

--- CONNECT/src/main/java/com/connect/CONNECT/ConnectApplication.java
+++ CONNECT/src/main/java/com/connect/CONNECT/ConnectApplication.java
@@ -2,6 +2,8 @@ package com.connect.CONNECT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConnectApplication {
@@ -10,4 +12,9 @@ public class ConnectApplication {
 		SpringApplication.run(ConnectApplication.class, args);
 	}
 
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
--- CONNECT/src/main/java/com/connect/CONNECT/controller/PublicController.java
+++ CONNECT/src/main/java/com/connect/CONNECT/controller/PublicController.java
@@ -2,8 +2,10 @@ package com.connect.CONNECT.controller;
 
 
 import com.connect.CONNECT.entry.User;
+import com.connect.CONNECT.exceptions.WeatherServiceException;
 import com.connect.CONNECT.service.UserRepositoryIMPL;
 import com.connect.CONNECT.service.UserService;
+import com.connect.CONNECT.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,9 @@ import java.util.List;
 @RequestMapping("/public")
 public class PublicController {
 
+
+    Integer temperature;
+    List<String > weather_description;
     private static final String HEALTH_CHECK_SUCCESS = "OK";
 
     @Autowired
@@ -23,6 +28,9 @@ public class PublicController {
 
     @Autowired
     private UserRepositoryIMPL userRepositoryIMPL;
+
+    @Autowired
+    private WeatherService weatherService;
 
 
     @GetMapping("/health-check")
@@ -45,4 +53,20 @@ public class PublicController {
     public List<User> getSentiment(){
         return userRepositoryIMPL.getUserForSA();
     }
+
+
+
+    @GetMapping("/weather-check/{city}")
+    public ResponseEntity<?> weatherCheck(@PathVariable String city){
+        try{
+            temperature = weatherService.getWeather(city).getCurrent().getTemperature();
+            weather_description = weatherService.getWeather(city).getCurrent().getWeatherDescriptions();
+            return new ResponseEntity<>(temperature+" "+weather_description,HttpStatus.OK);
+        } catch (WeatherServiceException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
 }
